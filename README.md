54a02b189f708ec7a83dc61cccbf2421c086049b
Styla <117137871+stylarene@users.noreply.github.com>
2023-04-05 19:00:20 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# WhatsApp-ChatGTP
-Let ChatGTP answer for you in WhatsApp or use a second WhatsApp Account to communicate with ChatGTP via WhatsApp chat permanently
+whatsapp-Chatgpt
+
+* Let ChatGTP answer for you in WhatsApp or use a second WhatsApp Account to communicate with ChatGTP via WhatsApp chat permanently.
+
+*Install:
+Python: https://www.python.org/
+Go: https://go.dev/dl/
+GCC Compiler: https://jmeubank.github.io/tdm-gcc/
+
+* Install the necessary modules like *flask, *openai, *playwright tldr...
+* Edit Update.py in Line: (openai.api_key =) with your own api-Key
+* Open two terminals: QR-Code:"go run Update.go" and Server:"python Update.py"
+* Scan QR Code with WhatsApp and have fun.
+
+
---
+++ Update.py
@@ -0,0 +1,28 @@
+"""Make some requests to OpenAI's chatbot"""
+
+import flask
+import sys
+import openai
+
+from flask import g
+
+
+PORT = 5001 if '--port' not in sys.argv else int(sys.argv[sys.argv.index('--port') + 1])
+APP = flask.Flask(__name__)
+openai.api_key = 'YOUR-API-KEY'
+
+
+@APP.route("/chat", methods=["GET"])
+def chat():
+    message = flask.request.args.get("q")
+    print("Sending message: ", message)
+    response = openai.Completion.create(model="text-davinci-003", prompt=""+ message, max_tokens=200,top_p=0.0,frequency_penalty=0.5,presence_penalty=0.0)
+    print("Response: ", response)
+    return response.choices[0].text
+
+def startAI():
+    APP.run(port=PORT, threaded=False)
+
+        
+if __name__ == "__main__":
+    startAI()
